789fcb5a7589e3c4d26e58eedb3e4d9396ab305b
95degree <95degree@naver.com>
2020-12-08 23:47:18 +0900

    revise method name

--- src/WordMove.java
+++ src/WordMove.java
@@ -13,7 +13,7 @@ public class WordMove {
 
     private void init(String word, int count) {
         if (count < 0) this.direction = changeDirection();
-        MoveWordArray(Math.abs(count));
+        moveWordArray(Math.abs(count));
     }
 
     private String changeDirection() {
@@ -22,24 +22,24 @@ public class WordMove {
         return "L";
     }
 
-    private void MoveWordArray(int count) {
+    private void moveWordArray(int count) {
 
         for (int i = 0; i < count; i++) {
             if (this.direction.equals("R"))
-                MoveRight();
+                moveRight();
             if (this.direction.equals("L"))
-                MoveLeft();
+                moveLeft();
         }
     }
 
-    private void MoveRight() {
+    private void moveRight() {
         String save = this.wordArray[this.length - 1];
         for (int j = this.length - 1; j > 0; j--)
             this.wordArray[j] = this.wordArray[j - 1];
         this.wordArray[0] = save;
     }
 
-    private void MoveLeft() {
+    private void moveLeft() {
         String save = this.wordArray[0];
         for (int j = 0; j < this.length - 1; j++)
             this.wordArray[j] = this.wordArray[j + 1];
