964576ccf73224cd961dfaf83f55ff96d28d1016
95degree <95degree@naver.com>
2020-12-08 16:14:20 +0900

    rewrite WorldMove.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        WordMove wordMove = new WordMove(input.next());
+        System.out.print("> ");
+        String word = input.next();
+        int count = input.nextInt();
+        String direction = input.next();
+        WordMove wordMove = new WordMove(word, count, direction);
     }
 }
--- src/WordMove.java
+++ src/WordMove.java
@@ -1,22 +1,32 @@
 public class WordMove {
-    char[] wordArray;
+    private char[] wordArray;
+    private String direction;
 
-    public WordMove(String inputArray) {
-        String[] splitArray = inputArray.split(" ");
-        String word = splitArray[0];
-        int count = Integer.parseInt(splitArray[1]);
-        String direction = splitArray[2];
-        init(word, count, direction);
+    public WordMove(String word, int count, String direction) {
+        this.direction = direction.toUpperCase();
+        init(word, count);
+        print();
     }
 
-    private void init(String word, int count, String direction) {
+    private void init(String word, int count) {
+        if (count < 0) this.direction = changeDirection();
         MakeWordArray(word);
     }
 
-    private void MakeWordArray(String word){
-        for(int i=0; i< word.length(); i++){
-            this.wordArray[i]=word.charAt(i);
+    private void MakeWordArray(String word) {
+        for (int i = 0; i < word.length(); i++) {
+            this.wordArray[i] = word.charAt(i);
         }
     }
 
+    private String changeDirection() {
+        if (this.direction.equals("L"))
+            return "R";
+        return "L";
+    }
+
+    private void print(){
+        for(char a : this.wordArray)
+            System.out.println(a);
+    }
 }
