9da56091f5170a4488a4d0ea3d4d84b3a0cb43b3
95degree <95degree@naver.com>
2020-12-12 00:15:19 +0900

    complete OutputCube.java

--- src/CubeMain.java
+++ src/CubeMain.java
@@ -3,8 +3,8 @@ public class CubeMain {
     public void init(){
         System.out.println("1:기본 큐브, 2:랜덤 큐브");
         CommandInput input = new CommandInput();
-        CubePlane cube = new CubePlane(input.getCommand());
         OutputCube print = new OutputCube();
+        CubePlane cube = new CubePlane(input.getCommand());
         print.printCube(cube.getCube());
     }
 }
