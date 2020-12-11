dc68f1c3c4ccd331ff127d7add51b920b54cb65d
95degree <95degree@naver.com>
2020-12-11 15:59:24 +0900

    add makeNormalPlane, makeColorArray method

--- src/Cube.java
+++ src/Cube.java
@@ -1,10 +1,2 @@
 public class Cube {
-    private static final String[] COLOR = {"B", "W", "O", "G", "Y", "R"};
-    CubePlane top, front, right, left, back, bottom;
-
-    public Cube(CubePlane top, CubePlane front, CubePlane right, CubePlane left,
-                CubePlane back, CubePlane bottom) {
-
-
-    }
 }
--- src/CubePlane.java
+++ src/CubePlane.java
@@ -1,15 +1,38 @@
-public class CubePlane {
-    private static final int SIZE = 3;
-    private String[][] cube;
+import java.util.HashMap;
 
-    public CubePlane(String color) {
-        this.cube = new String[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++)
-            for (int j = 0; j < SIZE; j++)
-                this.cube[i][j] = color;
+public class CubePlane {
+    private static final String[] COLOR = {"B", "W", "O", "G", "Y", "R"};
+    private static final String[] DIRECTION = {"top", "front", "right", "left", "back", "bottom"};
+    private static final int SIZE = 3;
+    private HashMap<String, String[][]> cube;
+
+    public CubePlane(String command) {
+        if (command.equals("1"))
+            makeNormalPlane();
+        if (command.equals("2"))
+            makeShufflePlane();
     }
 
-    public String[][] getCube() {
+    private void makeNormalPlane() {
+        for(String s : DIRECTION)
+            for (String a : COLOR)
+                this.cube.put(s,makeColorArray(a));
+    }
+
+    private String[][] makeColorArray(String color) {
+        String[][] colorPlane = new String[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++)
+            for (int j = 0; j < SIZE; j++) {
+                colorPlane[i][j] = color;
+            }
+        return colorPlane;
+    }
+
+    private void makeShufflePlane() {
+
+    }
+
+    public HashMap<String, String[][]> getCube(){
         return this.cube;
     }
 }
